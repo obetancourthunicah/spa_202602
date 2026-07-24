72fad6597fffad52c373bae07baf2731c6647f4e
Orlando J Betancourth <obetancourthunicah@gmail.com>
2026-07-23 19:36:19 -0600

    fix: cambio de english a español

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router";
 const NotFoundPage = () => {
   return (
     <section className="page">
-      <h2 className="page-title">Page Not Found</h2>
-      <p className="page-text">The requested route does not exist.</p>
+      <h2 className="page-title">Pagina no encontrada</h2>
+      <p className="page-text">La pagina solicitada no se encuentra o no existe</p>
       <p className="page-text">Un mensaje adicional a esto</p>
       <Link to="/home" className="nav-link">
-        Back to Home
+        Regresar
       </Link>
     </section>
   );
